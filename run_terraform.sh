de727c34f385aac3aa124f48c5831aea489c0812
sean-freeman <sean.freeman@outlook.com>
2022-06-24 09:44:16 +0100

    fix: wrong var name for opt

--- run_terraform.sh
+++ run_terraform.sh
@@ -70,7 +70,7 @@ function sap_solution_scenario_choice()
     do
         case $opt_scenario in
             "SAP HANA - Install single node")
-                echo ">>> Chosen option $REPLY: $opopt_scenariot"
+                echo ">>> Chosen option $REPLY: $opt_scenario"
                 sap_solution_scenario="sap_hana_single_node_install"
                 break
                 ;;
