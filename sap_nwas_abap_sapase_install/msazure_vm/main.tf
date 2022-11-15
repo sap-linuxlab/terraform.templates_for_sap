9b84522e9fc27baafe10f699b03bd1e5adb562c0
sean-freeman <sean.freeman@outlook.com>
2022-11-15 13:46:09 +0000

    fix: update vars for sg rules on azure

--- sap_nwas_abap_sapase_install/msazure_vm/main.tf
+++ sap_nwas_abap_sapase_install/msazure_vm/main.tf
@@ -134,6 +134,9 @@ module "run_host_network_access_sap_module" {
 
   module_var_host_security_group_name = module.run_account_bootstrap_module.output_host_security_group_name
 
+  module_var_sap_nwas_abap_pas_instance_no = var.sap_nwas_abap_pas_instance_no
+  module_var_sap_hana_instance_no     = ""
+
 }
 
 
@@ -158,7 +161,7 @@ module "run_host_network_access_sap_public_via_proxy_module" {
   module_var_bastion_connection_security_group_name = module.run_bastion_inject_module.output_bastion_connection_security_group_name
 
   module_var_sap_nwas_abap_pas_instance_no = var.sap_nwas_abap_pas_instance_no
-  module_var_sap_hana_instance_no     = var.sap_hana_install_instance_number
+  module_var_sap_hana_instance_no     = ""
 
 }
 
