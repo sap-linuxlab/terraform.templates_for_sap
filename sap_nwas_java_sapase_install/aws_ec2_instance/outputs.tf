626720384b7e48aa10fc9b24129fafb5cb854c67
sean-freeman <sean.freeman@outlook.com>
2022-11-15 04:31:13 +0000

    fix: outputs for nwas on aws

--- sap_nwas_java_sapase_install/aws_ec2_instance/outputs.tf
+++ sap_nwas_java_sapase_install/aws_ec2_instance/outputs.tf
@@ -45,21 +45,20 @@ bastion_host="${module.run_bastion_inject_module.output_bastion_ip}"
 bastion_port="${var.bastion_ssh_port}"
 target_host_array=(${join(" ", flatten([for key, value in module.run_host_provision_module : value.*.output_host_private_ip]))} "Quit")
 
-sap_hana_instance_no="${var.sap_hana_install_instance_number}"
-sap_nwas_abap_pas_instance_no="${var.sap_nwas_abap_pas_instance_no}"
+sap_nwas_java_ci_instance_no="${var.sap_nwas_java_ci_instance_no}"
 
 
 function sshjump() {
 
     ssh_options=(
-        "SAP HANA Studio or SAPGUI, via SSH port forward binding proxy"
+        "SAPGUI, via SSH port forward binding proxy"
         "OS root access, via SSH stdin/stdout forwarding proxy"
         "Quit"
     )
 
     select opt in "$${ssh_options[@]}"; do
         case $opt in
-        "SAP HANA Studio or SAPGUI, via SSH port forward binding proxy")
+        "SAPGUI, via SSH port forward binding proxy")
             echo ">>> Chosen option $REPLY: $opt"
             select opt_ip in "$${target_host_array[@]}"; do
                 if [ $opt_ip = "Quit" ]; then break 2; fi
@@ -68,25 +67,14 @@ function sshjump() {
                 break
             done
             echo ""
-            echo "#### For SAP HANA Studio, use Add System with host name as localhost; do not add port numbers."
-            echo "#### If selecting 'Connect using SSL' on Connection Properties, then on Additional Properties (final) screen deselect 'Validate the SSL certificate'"
-            echo ""
-            echo "#### For SAPGUI, use expert mode SAP Logon String as: ####"
-            echo "conn=/H/localhost/S/32$sap_nwas_abap_pas_instance_no&expert=true"
-            echo ""
+            echo "#### HTTP/S JAVA endpoints: ####"
             # SSH port forward binding, using -L local_host:local_port:remote_host:remote_port (add -vv for debugging)
             ssh -N \
                 $bastion_user@$bastion_host -p $bastion_port -i $bastion_private_key_file \
                 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-                -L localhost:32$sap_nwas_abap_pas_instance_no:$target_ip:32$sap_nwas_abap_pas_instance_no \
-                -L localhost:33$sap_nwas_abap_pas_instance_no:$target_ip:33$sap_nwas_abap_pas_instance_no \
-                -L localhost:3$${sap_hana_instance_no}13:$target_ip:3$${sap_hana_instance_no}13 \
-                -L localhost:3$${sap_hana_instance_no}15:$target_ip:3$${sap_hana_instance_no}15 \
-                -L localhost:3$${sap_hana_instance_no}41:$target_ip:3$${sap_hana_instance_no}41 \
-                -L localhost:443$sap_hana_instance_no:$target_ip:443$sap_hana_instance_no \
-                -L localhost:443$sap_nwas_abap_pas_instance_no:$target_ip:443$sap_nwas_abap_pas_instance_no \
-                -L localhost:5$${sap_hana_instance_no}13:$target_ip:5$${sap_hana_instance_no}13 \
-                -L localhost:5$${sap_hana_instance_no}14:$target_ip:5$${sap_hana_instance_no}14
+                -L localhost:5$${sap_nwas_java_ci_instance_no}00:$target_ip:5$${sap_nwas_java_ci_instance_no}01 \
+                -L localhost:5$${sap_nwas_java_ci_instance_no}20:$target_ip:5$${sap_nwas_java_ci_instance_no}20 \
+                -L localhost:5$${sap_nwas_java_ci_instance_no}13:$target_ip:5$${sap_nwas_java_ci_instance_no}14
             break
             ;;
         "OS root access, via SSH stdin/stdout forwarding proxy")
@@ -117,6 +105,7 @@ sshjump
 
 EOF
 }
+
 
 # If detected Windows WSL2, then find the installed name using external resource to return a JSON string
 data "external" "wsl_distro_name" {
@@ -152,13 +141,12 @@ $bastion_port = "${var.bastion_ssh_port}"
 $target_host_string = "${join("','",flatten([for key, value in module.run_host_provision_module : value.*.output_host_private_ip]))}"
 $target_host_array = @($target_host_string.Split(","),"Quit")
 
-$sap_hana_instance_no = "${var.sap_hana_install_instance_number}"
-$sap_nwas_abap_pas_instance_no = "${var.sap_nwas_abap_pas_instance_no}"
+$sap_nwas_java_ci_instance_no = "${var.sap_nwas_java_ci_instance_no}"
 
 
 function sshjump {
 
-    echo '1) SAP HANA Studio or SAPGUI, via SSH port forward binding proxy'
+    echo '1) SAPGUI, via SSH port forward binding proxy'
     echo '2) OS root access, via SSH stdin/stdout forwarding proxy'
     echo '3) Quit'
 
@@ -186,25 +174,14 @@ function sshjump {
                 $target_ip = $target_host_array[$target_host_selection]
                 #echo ">>> Chosen option $(PSItem)"
                 echo ""
-                echo "#### For SAP HANA Studio, use Add System with host name as localhost; do not add port numbers."
-                echo "#### If selecting 'Connect using SSL' on Connection Properties, then on Additional Properties (final) screen deselect 'Validate the SSL certificate'"
-                echo ""
-                echo "#### For SAPGUI, use expert mode SAP Logon String as: ####"
-                echo "conn=/H/localhost/S/32$sap_nwas_abap_pas_instance_no&expert=true"
-                echo ""
+                echo "#### HTTP/S JAVA endpoints: ####"
                 # SSH port forward binding, using -L local_host:local_port:remote_host:remote_port (add -vv for debugging)
                 ssh -N `
                 $bastion_user@$bastion_host -p $bastion_port -i $temp_bastion_private_key_file `
                 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null `
-                -L localhost:32$${sap_nwas_abap_pas_instance_no}:$${target_ip}:32$${sap_nwas_abap_pas_instance_no} `
-                -L localhost:33$${sap_nwas_abap_pas_instance_no}:$${target_ip}:33$${sap_nwas_abap_pas_instance_no} `
-                -L localhost:3$${sap_hana_instance_no}13:$${target_ip}:3$${sap_hana_instance_no}13 `
-                -L localhost:3$${sap_hana_instance_no}15:$${target_ip}:3$${sap_hana_instance_no}15 `
-                -L localhost:3$${sap_hana_instance_no}41:$${target_ip}:3$${sap_hana_instance_no}41 `
-                -L localhost:443$${sap_hana_instance_no}:$${target_ip}:443$${sap_hana_instance_no} `
-                -L localhost:443$${sap_nwas_abap_pas_instance_no}:$${target_ip}:443$${sap_nwas_abap_pas_instance_no} `
-                -L localhost:5$${sap_hana_instance_no}13:$${target_ip}:5$${sap_hana_instance_no}13 `
-                -L localhost:5$${sap_hana_instance_no}14:$${target_ip}:5$${sap_hana_instance_no}14
+                -L localhost:5$${sap_nwas_java_ci_instance_no}00:$${target_ip}:5$${sap_nwas_java_ci_instance_no}01 `
+                -L localhost:5$${sap_nwas_java_ci_instance_no}20:$${target_ip}:5$${sap_nwas_java_ci_instance_no}20 `
+                -L localhost:5$${sap_nwas_java_ci_instance_no}13:$${target_ip}:5$${sap_nwas_java_ci_instance_no}14
             }
         }
         2 {
