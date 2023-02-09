070d3e6e899b903554f640305cff4ccf8844f2b5
sean-freeman <sean.freeman@outlook.com>
2023-02-09 13:26:05 +0000

    fix: gcp tf modules ref

--- sap_hana_single_node_install/gcp_ce_vm/main.tf
+++ sap_hana_single_node_install/gcp_ce_vm/main.tf
@@ -1,7 +1,7 @@
 
 module "run_account_init_module" {
 
-  source = "github.com/sap-linuxlab/terraform.modules_for_sap//all/gcp_ce_vm/account_init?ref=dev"
+  source = "github.com/sap-linuxlab/terraform.modules_for_sap//gcp_ce_vm/account_init?ref=dev"
 
   module_var_resource_prefix                = var.resource_prefix
 
@@ -18,7 +18,7 @@ module "run_account_bootstrap_module" {
     module.run_account_init_module
   ]
 
-  source = "github.com/sap-linuxlab/terraform.modules_for_sap//all/gcp_ce_vm/account_bootstrap?ref=dev"
+  source = "github.com/sap-linuxlab/terraform.modules_for_sap//gcp_ce_vm/account_bootstrap?ref=dev"
 
   module_var_resource_prefix                = var.resource_prefix
 
@@ -35,7 +35,7 @@ module "run_bastion_inject_module" {
     module.run_account_bootstrap_module
   ]
 
-  source = "github.com/sap-linuxlab/terraform.modules_for_sap//all/gcp_ce_vm/bastion_inject?ref=dev"
+  source = "github.com/sap-linuxlab/terraform.modules_for_sap//gcp_ce_vm/bastion_inject?ref=dev"
 
   module_var_resource_prefix                = var.resource_prefix
 
@@ -59,7 +59,7 @@ module "run_host_network_access_sap_module" {
     module.run_bastion_inject_module
   ]
 
-  source = "github.com/sap-linuxlab/terraform.modules_for_sap//all/gcp_ce_vm/host_network_access_sap?ref=dev"
+  source = "github.com/sap-linuxlab/terraform.modules_for_sap//gcp_ce_vm/host_network_access_sap?ref=dev"
 
   module_var_resource_prefix                = var.resource_prefix
 
@@ -76,7 +76,7 @@ module "run_host_network_access_sap_public_via_proxy_module" {
     module.run_bastion_inject_module
   ]
 
-  source = "github.com/sap-linuxlab/terraform.modules_for_sap//all/gcp_ce_vm/host_network_access_sap_public_via_proxy?ref=dev"
+  source = "github.com/sap-linuxlab/terraform.modules_for_sap//gcp_ce_vm/host_network_access_sap_public_via_proxy?ref=dev"
 
   module_var_resource_prefix                = var.resource_prefix
 
@@ -95,7 +95,7 @@ module "run_host_provision_module" {
     module.run_bastion_inject_module
   ]
 
-  source = "github.com/sap-linuxlab/terraform.modules_for_sap//all/gcp_ce_vm/host_provision?ref=dev"
+  source = "github.com/sap-linuxlab/terraform.modules_for_sap//gcp_ce_vm/host_provision?ref=dev"
 
 
   module_var_resource_prefix                = var.resource_prefix
